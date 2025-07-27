b10e7da976568b0fa3a04b8bf6938f3d65365a38
Bai1026 <1132abc30@gmail.com>
2025-07-27 17:50:29 +0000

    leave index.html alone

--- In_context_self_play/test_code/test_gemini.py
+++
@@ -1,6 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyCtE0PMaJyjeZLHfIp4s3kzStj0q0vg8vU")
-
-response = client.models.generate_content(model="gemini-2.0-flash", contents=["How does AI work?"])
-print(response.text)
--- In_context_self_play/utils/openai_token.py
+++
@@ -1,6 +0,0 @@
-def openai_token(response):
-    print('===============================')
-    print(f"Completion token: {response.usage.completion_tokens}")
-    print(f"Prompt token: {response.usage.prompt_tokens}")
-    print(f"Total token: {response.usage.total_tokens}")
-    print('===============================')
